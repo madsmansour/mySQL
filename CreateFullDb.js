f292e637864d6f21dbb46322fc824a572e2fb0a1
madsmansour <42993941+madsmansour@users.noreply.github.com>
2019-10-30 10:52:27 +0100

    134

--- CreateFullDb.js
+++ CreateFullDb.js
@@ -4,12 +4,32 @@ const insert = require('./mySQL_insert.js');
 const mysql = require('mysql2');
 
 
-const setup = async function() {
-    createDB();
-    const table = await createDB();
-    table.createTable();
-    const insert = await createTable();
-    insert.insert();
-} 
-setup();
+async function f() {
 
+    let promise1 = new Promise((resolve, reject) => {
+        createDB();
+        setTimeout(() => resolve("done1!"), 1000)
+    });
+
+    let promise2 = new Promise((resolve, reject) => {
+        createTable();
+        setTimeout(() => resolve("done5!"), 5000)
+    });
+
+    let promise3 = new Promise((resolve, reject) => {
+        insert();
+        setTimeout(() => resolve("done3!"), 3000)
+    });
+
+    let result =  promise1; // wait till the promise resolves (*)
+    result +=  promise2; // wait till the promise resolves (*)
+    result += promise3; // wait till the promise resolves (*)
+    console.log("Async " + result); // "done!"
+
+    result = await promise1; // wait till the promise resolves (*)
+    result += await promise2; // wait till the promise resolves (*)
+    result += await promise3; // wait till the promise resolves (*)
+    console.log("Syns " + result); // "done!"
+}
+
+f();
