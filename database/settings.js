ea4d628aa828f01d6e334ed3f1603c6ac04350f4
madsmansour <42993941+madsmansour@users.noreply.github.com>
2019-11-05 10:59:57 +0100

    install.js working plus inserting dummy table (not from mySQL)

--- database/settings.js
+++
@@ -1,8 +0,0 @@
-const settings = {
-    host: "192.168.99.101",
-    user: "root",
-    password: "mads",
-    database: "databaseKunder"
-};
-
-module.exports = settings;
---
+++ model/settings.js
@@ -0,0 +1,13 @@
+const settings = {
+    host: "192.168.99.101",
+    user: "root",
+    password: "mads",
+};
+
+const extra = {
+    database: "databaseKunder",
+    table: "kunder"
+}
+
+
+module.exports = {settings,extra};
